1cc31abac6be7058ebeeae58dbd7386760d9df73
Konstantin Ponamorev <kostyaponamorev@gmail.com>
2022-04-07 14:12:14 +0300

    Database?

---
+++ database.py
@@ -0,0 +1,47 @@
+from sqlalchemy import MetaData, Table, String, Integer, Column, ForeignKey, insert
+import sqlalchemy
+
+
+
+class DataBase:
+
+    def __init__(self):
+        self.engine = sqlalchemy.create_engine('postgresql://postgres:postgre@localhost:5432/postgres')
+        self.connection = self.engine.connect()
+        self.metadata = MetaData()
+        self.metadata.drop_all(self.engine)
+
+        user = Table('user', self.metadata,
+                       Column('id', Integer(), primary_key=True),
+                       Column('age', Integer(), nullable=False),
+                       Column('sex', String(), nullable=False),
+                       Column('city_id', Integer(), nullable=False)
+                        )
+
+        match = Table('match', self.metadata,
+                        Column('id', Integer(), primary_key=True),
+                        Column('url', String(), nullable=False),
+                        Column('photo1_url', String()),
+                        Column('photo2_url', String()),
+                        Column('photo3_url', String())
+                        )
+
+        user_match = Table('user_match', self.metadata,
+                               Column('user_id', ForeignKey('user.id')),
+                               Column('match_id', ForeignKey('match.id'))
+                               )
+
+        self.metadata.create_all(self.engine)
+
+    def insert_user(self, user_info):
+        self.ins_user = insert(user)
+        self.connection.execute(self.ins_user, user_info)
+
+    def insert_desired(self, match_info):
+        self.ins_match = insert(match)
+        self.connection.execute(self.ins_match, match_info)
+
+
+
+
+# db = DataBase()
